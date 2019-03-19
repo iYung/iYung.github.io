7419f10a0df25902652a958e279964227936aef3
iYung <ivankhyung@gmail.com>
2019-03-19 02:54:49 -0400

    Updates

---
+++ precache-manifest.fed5022058478dcee3abba80bef8ad68.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1756cd1179afba98a973",
+    "url": "/static/js/main.8ef9628d.chunk.js"
+  },
+  {
+    "revision": "da812a7baf9878a93b19",
+    "url": "/static/js/2.a501cce7.chunk.js"
+  },
+  {
+    "revision": "fc2ac6437a792b26d95010c41573ee92",
+    "url": "/index.html"
+  }
+];
